269617beade3a49550d585f4d1b50d317492fb7d
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-02 15:20:33 +0900

    Add the background.

---
+++ Shared/Views/Card.swift
@@ -0,0 +1,24 @@
+//
+//  Card.swift
+//  SinglePriceGridComponent (iOS)
+//
+//  Created by Jinwook Kim on 2022/04/02.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct Card_Previews: PreviewProvider {
+    static var previews: some View {
+        Card()
+            .padding()
+            .previewLayout(.sizeThatFits)
+    }
+}
