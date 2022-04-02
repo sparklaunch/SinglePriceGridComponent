b2653a2d88ae24c0011de9ee28bfda8c19dc5bb2
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-02 15:10:48 +0900

    Initial Commit

---
+++ Shared/SinglePriceGridComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinglePriceGridComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/02.
+//
+
+import SwiftUI
+
+@main
+struct SinglePriceGridComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
